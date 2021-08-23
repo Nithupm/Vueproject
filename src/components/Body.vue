794e2f13c92d05c36b7370bc606a8ea8b95ab6c2
Nithupm <nithupm8@gmail.com>
2021-08-23 20:49:28 +0530

    body

---
+++ src/components/Body.vue
@@ -0,0 +1,139 @@
+<template>
+    <v-main class="root" >
+    <div>
+    <div class="holiday">
+        
+    <hr class="line"/>
+    <h2 class="list">HOLIDAY LIST - 2021</h2>
+    <hr class="line1"/>
+    <hr class="line2"/>
+    <h3 class="title">HOLIDAYS</h3>
+    <hr class="line3"/>
+    <ul class="lis">
+        <li class="new">NEW YEAR</li>
+        <li> MAKARA SANKRANTHI</li>
+        <li>REPUBLIC DAY</li>
+        <li>UGADI</li>
+        <li>MAYDAY</li>
+        <li>RAMZAN</li>
+        <li>INDEPENDENCE DAY</li>
+        <li>GANESH CHATURTHI</li>
+        <li>GANDHI JAYANTHI</li>
+        <li>VIJAYA DASHAMI/DUSSEHRA</li>
+        <li>DEEPAVALI</li>
+        <li>CHRISTMAS</li>
+        
+   </ul>
+</div>
+<hr class="lin"/>
+<div class="p-des">
+  <p class="p-note">NOTE: IF REQUIRED, YOU NEED TO WORK ON THESE DAYS BUT CAN TAKE COMPENSATORY OFF ANY OTHER DAY</p>
+</div>
+
+<div class="date">
+    <h3 class="year">2021</h3>
+    <ul class="list1">
+        <li class="fri">FRIDAY,01 JANUARY</li>
+        <li>THURSDAY,14 JANUARY</li>
+        <li>TUESDAY,26 JANUARY</li>
+        <li>TUESDAY, 13 APRIL</li>
+        <li>SATURDAY,01 MAY</li>
+        <li>FRIDAY,14 MAY</li>
+        <li>SUNDAY,15 AUGUST</li>
+        <li>FRIDAY,10 SEPTEMBER</li>
+        <li>SATURDAY,02 OCTOBER</li>
+        <li>FRIDAY,15 OCTOBER</li>
+        <li>FRIDAY,05 NOVEMBER</li>
+        <li>SATURDAY,25 DECEMBER</li>
+    </ul>
+</div>
+</div>
+</v-main>
+</template>
+
+<style>
+.root{
+    background-image: url(https://us.123rf.com/450wm/melpomen/melpomen1810/melpomen181001692/109922889-beautiful-abstract-shiny-light-and-glitter-background.jpg?ver=6);
+    background-size: cover;
+    height: 727px;
+}
+
+.line{
+    background-color: green;
+    max-width: 1425px;
+    margin-left: 44px;
+    margin-top: 45px;
+}
+.list{
+    margin-left: 603px;
+    margin-top: 23px;
+}
+.title{
+    margin-left: 180px;
+}
+.lis{
+    margin-left: 80px;
+}
+
+.line3{
+    background-color: green;
+    max-width: 1425px;
+    margin-left: 44px;
+    height: 1px;
+}
+.year{
+    margin-left: 1056px;
+    margin-top: -486px;
+}
+.list1{
+    margin-left: 967px;
+    
+    margin-top: 7px;
+}
+.hor{
+    margin-top: -1357px;
+    height: 457px;
+    width: 2px
+
+}
+.line2{
+    margin-top: 88px;
+    background-color: green;
+    max-width: 1425px;
+    margin-left: 44px;
+    height: 3px;
+}
+.line1{
+    margin-top: 38px;
+    max-width: 1425px;
+    margin-left: 44px;
+    background-color: green;
+}
+
+.holiday1{
+    margin-top: 299px;
+}
+.p-des{
+    margin-left: 104px;
+    width: 302px;
+}
+.p-note{
+    color: green;
+    font-style: italic;
+    font-size: small;
+    font-weight: bold;
+    margin-top: 44px;
+}
+.fri{
+    margin-top: 18px;
+}
+.new{
+    margin-top: 10px;
+}
+.lin{
+    margin-top: 36px;
+    background-color: green;
+    max-width: 1425px;
+    margin-left: 44px;
+}
+</style>
